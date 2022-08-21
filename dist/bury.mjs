0bbd23cfbd1e3ac07e08e75524a47790f7c39baf
mtsgi <oq@live.jp>
2022-08-21 16:52:05 +0900

    add types to argument functions

--- dist/bury.mjs
+++ dist/bury.mjs
@@ -1,23 +1,25 @@
-class i {
+class s {
   constructor() {
-    const t = {
-      Array: i.ARRAY_METHODS(),
-      String: i.STRING_METHODS(),
-      Number: i.NUMBER_METHODS()
-    }, r = {
+    const r = ["Array", "String", "Number"], t = {
+      Array: s.ARRAY_METHODS(),
+      String: s.STRING_METHODS(),
+      Number: s.NUMBER_METHODS()
+    }, e = {
       Array: Array.prototype,
       String: String.prototype,
       Number: Number.prototype
     };
-    for (const e in t)
-      for (const n in t[e])
-        t[e][n].length === 0 ? Object.defineProperty(r[e], n, {
+    r.forEach((n) => {
+      const u = t[n];
+      for (const i in u)
+        t[n][i].length === 0 ? Object.defineProperty(e[n], i, {
           configurable: !0,
           enumerable: !1,
           get() {
-            return t[e][n].apply(this);
+            return t[n][i].apply(this);
           }
-        }) : r[e][n] = t[e][n];
+        }) : e[n][i] = t[n][i];
+    });
   }
   static ARRAY_METHODS() {
     return {
@@ -28,46 +30,46 @@ class i {
         return this[this.length - 1];
       },
       min() {
-        return this.reduce((t, r) => Math.min(t, r));
+        return this.reduce((r, t) => Math.min(r, t));
       },
       max() {
-        return this.reduce((t, r) => Math.max(t, r));
+        return this.reduce((r, t) => Math.max(r, t));
       },
       sum() {
-        return this.reduce((t, r) => Number(t) + Number(r));
+        return this.reduce((r, t) => Number(r) + Number(t));
       },
-      append(t) {
+      append(r) {
         return this.push(...arguments), this;
       },
-      prepend(t) {
+      prepend(r) {
         return [...arguments, ...this];
       },
       compact() {
-        return this.reduce((t, r) => r ? t.append(r) : t, []);
+        return this.reduce((r, t) => t ? r.append(t) : r, []);
       },
       uniq() {
-        return this.reduce((t, r) => t.indexOf(r) === -1 ? t.append(r) : t, []);
+        return this.reduce((r, t) => r.indexOf(t) === -1 ? r.append(t) : r, []);
       },
-      each(t) {
-        return this.forEach((r) => t(r)), this;
+      each(r) {
+        return this.forEach((t) => r(t)), this;
       },
-      each_with_index(t) {
-        return this.forEach((r, e) => t(r, e)), this;
+      each_with_index(r) {
+        return this.forEach((t, e) => r(t, e)), this;
       },
-      delete(t) {
-        return this.reduce((r, e) => e !== t ? r.append(e) : r, []);
+      delete(r) {
+        return this.reduce((t, e) => e !== r ? t.append(e) : t, []);
       },
-      delete_if(t) {
-        return this.reduce((r, e) => t(e) ? r : r.append(e), []);
+      delete_if(r) {
+        return this.reduce((t, e) => r(e) ? t : t.append(e), []);
       },
-      delete_at(t) {
-        return this.splice(t, 1), this;
+      delete_at(r) {
+        return this.splice(r, 1), this;
       },
       clear() {
         return [];
       },
-      union(t) {
-        return this.concat(t).uniq;
+      union(r) {
+        return this.concat(r).uniq;
       },
       size() {
         return this.length;
@@ -75,20 +77,20 @@ class i {
       minmax() {
         return [this.min, this.max];
       },
-      min_by(t) {
-        return this.reduce((r, e) => t(r) < t(e) ? r : e);
+      min_by(r) {
+        return this.reduce((t, e) => r(t) < r(e) ? t : e);
       },
-      max_by(t) {
-        return this.reduce((r, e) => t(r) > t(e) ? r : e);
+      max_by(r) {
+        return this.reduce((t, e) => r(t) > r(e) ? t : e);
       },
-      find_all(t) {
-        return this.filter(t);
+      find_all(r) {
+        return this.filter(r);
       },
-      select(t) {
-        return this.find_all(t);
+      select(r) {
+        return this.find_all(r);
       },
-      count(t) {
-        return this.find_all(t).size;
+      count(r) {
+        return this.find_all(r).size;
       },
       rev() {
         return [...this].reverse();
@@ -112,14 +114,14 @@ class i {
       strip() {
         return this.trim();
       },
-      gsub(t, r) {
-        return this.replace(t, r);
+      gsub(r, t) {
+        return this.replace(r, t);
       },
-      center(t) {
-        return [" ".repeat(t), this, " ".repeat(t)].join("");
+      center(r) {
+        return [" ".repeat(r), this, " ".repeat(r)].join("");
       },
-      prepend(t) {
-        return [t, this].join("");
+      prepend(r) {
+        return [r, this].join("");
       },
       size() {
         return this.length;
@@ -158,17 +160,17 @@ class i {
       inspect() {
         return this.to_s;
       },
-      times(t) {
-        for (const r of [...Array(this).keys()])
-          t.apply(window, [r]);
+      times(r) {
+        for (const t of [...Array(this).keys()])
+          r.apply(window, [t]);
         return this;
       },
-      clamp(t, r) {
-        return Math.min(Math.max(this, t), r);
+      clamp(r, t) {
+        return Math.min(Math.max(this, r), t);
       }
     };
   }
 }
 export {
-  i as Bury
+  s as Bury
 };
--- types/main.d.ts
+++ types/main.d.ts
@@ -11,20 +11,20 @@ declare global {
     prepend(...el: T[]): Array<T>;
     compact: Array<T>;
     uniq: Array<T>;
-    each(f: Function): Array<T>;
-    each_with_index(f: Function): Array<T>;
+    each(f: (value: T) => any): Array<T>;
+    each_with_index(f: (value: T, index: number) => any): Array<T>;
     delete(target: T): Array<T>;
-    delete_if(f: Function): Array<T>;
+    delete_if(f: (value: T) => any): Array<T>;
     delete_at(pos: number): Array<T>;
     clear: [];
     union(other: Array<T>): Array<T>;
     size: number;
     minmax: Array<T>;
-    min_by(f: Function): T;
-    max_by(f: Function): T;
-    find_all(f: Function): Array<T>;
-    select(f: Function): Array<T>;
-    count(f: Function): number;
+    min_by(f: (value: T) => number): T;
+    max_by(f: (value: T) => number): T;
+    find_all(f: (value: T, index: number, array: T[]) => any): Array<T>;
+    select(f: (value: T, index: number, array: T[]) => any): Array<T>;
+    count(f: (value: T, index: number, array: T[]) => any): number;
     rev: Array<T>;
   }
 
@@ -51,7 +51,7 @@ declare global {
     prev_float: number;
     to_s: string;
     inspect: string;
-    times(f: Function): number;
+    times(f: (num: number) => any): number;
     clamp(min: number, max: number): number;
   }
 }
